72d8e336957e03ad2b8e537afba4f1c29e7c12fc
diff3 <magnus@entropy.nu>
2021-03-23 17:59:29 +0100

    areatriggers

--- database/model/dbc.py
+++ database/model/dbc.py
@@ -26,3 +26,14 @@ class TaxiNode(Base):
     Name_zhCN = Column(Text)
     Name_enTW = Column(Text)
     Name_Mask = Column(INTEGER(10), nullable=False, server_default=text("'0'"))
+
+
+class AreaTrigger(Base):
+    __tablename__ = 'AreaTrigger'
+
+    ID = Column(INTEGER(11), primary_key=True, server_default=text("'0'"))
+    ContinentID = Column(INTEGER(11), nullable=False, server_default=text("'0'"))
+    X = Column(Float, nullable=False, server_default=text("'0'"))
+    Y = Column(Float, nullable=False, server_default=text("'0'"))
+    Z = Column(Float, nullable=False, server_default=text("'0'"))
+    Radius = Column(Float, nullable=False, server_default=text("'0'"))
--- database/queryHandler.py
+++ database/queryHandler.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from database.model.realm import Characters
-from database.model.world import SpawnsCreatures
-from database.model.dbc import TaxiNode
+from database.model.world import *
+from database.model.dbc import TaxiNode, AreaTrigger
 from database.connection import ConnectDatabase
 from sqlalchemy import or_
 
@@ -55,12 +55,65 @@ class World:
 
         return lst
 
+    def get_worldport(self):
+        records = World.session.query(Worldports).filter(or_(
+            Worldports.map == '0', Worldports.map == '1')).all()
+
+        World.session.commit()
+
+        lst = list()
+
+        for record in records:
+            lst.append({
+                'position_x': record.x,
+                'position_y': record.y,
+                'map': record.map,
+            })
+
+        return lst
+
+    def get_gameobjects(self):
+        records = World.session.query(SpawnsGameobjects).filter(or_(
+            SpawnsGameobjects.spawn_map == '0', SpawnsGameobjects.spawn_map == '1')).all()
+
+        World.session.commit()
+
+        lst = list()
+
+        for record in records:
+            lst.append({
+                'position_x': record.spawn_positionX,
+                'position_y': record.spawn_positionY,
+                'map': record.spawn_map
+            })
+
+        return lst
+
+
+
 
 class Dbc:
     def __init__(self):
         Dbc.session = ConnectDatabase("alpha_dbc").connect()
 
     def get_taxi_nodes(self):
+        records = Dbc.session.query(TaxiNode).all()
+
+        Dbc.session.commit()
+
+        lst = list()
+
+        for record in records:
+            lst.append({
+                'position_x': record.X,
+                'position_y': record.Y,
+                'map': record.ContinentID,
+                'name': record.Name_enUS,
+            })
+
+        return lst
+
+    def get_area_triggers(self):
         records = Dbc.session.query(TaxiNode).all()
 
         Dbc.session.commit()
