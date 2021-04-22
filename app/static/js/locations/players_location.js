6cb4eb3671d6e0ca3479e780949506b4c0902b4c
diff3 <magnus@entropy.nu>
2021-04-22 22:24:49 +0200

    experimental code for positioning

--- app/static/js/locations/players_location.js
+++ app/static/js/locations/players_location.js
@@ -1,9 +1,61 @@
+function player_pos(positions) {
+  var pos_x, pos_y, size_x, size_y;
+  var xmin, xmax, ymin, ymax;
+
+  var xmin = -17000;
+  var xmax = 17000;
+
+  var ymin = -17000;
+  var ymax = 17000;
+
+  position = $("#world").position();
+
+  pos_x = position.left;
+  pos_y = position.top;
+
+  console.log("pos_x: " + pos_x)
+  console.log("pos_y: " + pos_y)
+
+  size_x = $("#world").width();
+  size_y = $("#world").height();
+
+  console.log("size_x: " +  size_x);
+  console.log("size_y: " +  size_y);
+
+  var player_x = positions['position_x'];
+  var player_y = positions['position_y'];
+
+  console.log("player_x: " + player_x);
+  console.log("player_y: " + player_y);
+
+  var player_xx = (player_x - xmin) / (xmax - xmin);
+  var player_yy = (player_y - ymin) / (ymax - ymin);
+
+  console.log("player_xx: " + player_xx);
+  console.log("plauer_yy: " + player_yy);
+
+  // posx = size_x * player_xx + 393;
+  // posy = size_y * player_yy + 25;
+
+   posx = (pos_x + size_x) * player_xx;
+   posy = (pos_y + size_y) * player_yy;
+
+  console.log("posx: " + posx);
+  console.log("posy: " + posy);
+
+  // return posy, posx - (posx * 2);
+  return posx, posy;
+}
+
 function players_location(position) {
   $("#world").empty();
 
   for (var i = 0; i < position.length; i++) {
     var locations = '<img src="/static/img/map/' + position[i]["faction"] + '.gif" class="player_info icon" id="' + i + '" onmouseover="divShow(event, \'' + position[i]['show'] + '\')" onmouseout="divHide(event,\'' + position[i]['show'] + '\')" \>'
+    var locations2 = '<img src="/static/img/map/group-icon.gif" class="player_info icon" id="a' + i + '" onmouseover="divShow(event, \'' + position[i]['show'] + '\')" onmouseout="divHide(event,\'' + position[i]['show'] + '\')" \>'
+
     $("#world").append(locations);
+    $("#world").append(locations2);
 
     // more to utils?
     $("#" + i).css({
